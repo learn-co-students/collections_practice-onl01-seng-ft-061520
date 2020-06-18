eb9a36c210b7c56b7579c606353a535147ee749d
Adam Workman <adammworkman25@gmail.com>
2020-06-18 17:34:09 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,5 +1,30 @@
+
+
 def sort_array_asc(int_arr)
   int_arr.sort
 end
 
-def sort_array_desc(str_arr)
+def sort_array_desc(int_arr)
+  int_arr.sort! {|a,b| b <=> a}
+end
+
+def sort_array_char_count(str_arr)
+  str_arr.sort! do |a,b|
+    a.length <=> b.length
+  end
+end
+
+def swap_elements(elements)
+  elements[1], elements[2] = elements [2], elements [1]
+  elements
+end
+
+def reverse_array(int_arr)
+  int_arr.reverse
+end
+
+def kesha_maker(str_arr)
+  str_arr.each do |name|
+    name[2] = "$"
+   end
+end
