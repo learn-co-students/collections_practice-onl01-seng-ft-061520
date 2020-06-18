7fa721905152793702530839aa9821872b4897a6
Adam Workman <adammworkman25@gmail.com>
2020-06-18 17:52:26 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -28,3 +28,21 @@ def kesha_maker(str_arr)
     name[2] = "$"
    end
 end
+
+def find_a(str_arr)
+  str_arr.find_all { |string| string.start_with?("a")}
+end
+
+def sum_array(int_arr)
+  int_arr.inject{|a,b| a + b}
+end
+
+def add_s(str_arr)
+  str_arr.each_with_index.collect do |string, index|
+    if index == 1  
+      string
+    else 
+      string + "s"
+    end
+  end
+end
