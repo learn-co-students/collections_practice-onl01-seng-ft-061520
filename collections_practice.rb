7772615780941ba496c8262fe8a48b428099e96f
Devon Wehrlin <dkwehrlin.99@gmail.com>
2020-06-19 20:17:29 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,45 @@
+def sort_array_asc(intergers)
+  intergers.sort {|a,b| a <=> b}
+end 
 
+def sort_array_desc(integers)
+  integers.sort {|a,b| b <=> a}
+end 
+
+def sort_array_char_count(strings)
+  strings.sort {|a,b| a.length <=> b.length}
+end
+
+def swap_elements(array)
+  array[1], array[2] = array[2], array[1]
+  return array
+end
+
+def reverse_array(integers)
+  integers.reverse 
+end
+
+def kesha_maker(string)
+  string.collect do |element| 
+    element[2] = "$"
+    element
+  end
+end
+
+def find_a(array)
+  array.select {|word| word.chr == "a"}
+end
+
+def sum_array(array)
+  array.inject {|sum, n| sum + n}
+end
+
+def add_s(array)
+  array.collect.with_index do |element, index|
+    if index == 1 
+      element
+    else
+      element + "s"
+    end
+  end
+end
